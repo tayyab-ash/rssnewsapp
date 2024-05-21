b7ac05026a15cf6225c61f7aacf49ffa985b7fd8
Tayyab <40110511+tayyab-ash@users.noreply.github.com>
2024-05-21 16:11:29 +0500

    new

--- src/Components/Home/Discover/DiscoverFeeds.js
+++ src/Components/Home/Discover/DiscoverFeeds.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import styles from './DiscoverFeeds.module.css'
+import React from "react";
+import styles from "./DiscoverFeeds.module.css";
 import { Link } from "react-router-dom";
 
 //images
-import techimg from "./assets/960x0.webp"
-import techimg2 from "./assets/WhatsApp Image 2024-05-13 at 11.18.24.jpeg"
+import techimg from "./assets/960x0.webp";
+import techimg2 from "./assets/WhatsApp Image 2024-05-13 at 11.18.24.jpeg";
 
 function DiscoverFeeds() {
   return (
     <div>
-        <div className={`${styles.discoverPg}`}>
+      <div className={`${styles.discoverPg}`}>
         <div className={`${styles.contentAreaHeading}`}>
           <h2>Discover</h2>
           <h4>Follow your favourite websites</h4>
@@ -18,17 +18,46 @@ function DiscoverFeeds() {
           <input type="text" />
           <button>Search</button>
         </div>
-        <div className={`${styles.catagories}`}>
-          
-          <div className={`${styles.card}`}>
-          <Link to="sources/tech"> <img  src={techimg2} alt="" /></Link> 
-            <span>TECH</span>
+        <div className={`${styles.catagories} row`}>
+          <div className={`${styles.card} col-lg-4 col-md-6`}>
+            <Link to="sources/tech" className={`${styles.imageContainer}`}>
+              {" "}
+              <div>
+                <div className={`img-wrap`}>
+                  <img src={techimg2} alt="" className="img-fluid" />
+                </div>
+              </div>
+              <span className={`${styles.centeredText}`}>TECH</span>
+            </Link>
           </div>
-        
+
+          <div className={`${styles.card} col-lg-4 col-md-6`}>
+            <Link to="sources/tech" className={`${styles.imageContainer}`}>
+              {" "}
+              <div>
+                <div className={`img-wrap`}>
+                  <img src={techimg2} alt="" className="img-fluid" />
+                </div>
+              </div>
+              <span className={`${styles.centeredText}`}>TECH</span>
+            </Link>
+          </div>
+
+          <div className={`${styles.card} col-lg-4 col-md-6`}>
+            <Link to="sources/tech" className={`${styles.imageContainer}`}>
+              {" "}
+              <div>
+                <div className="img-wrap">
+                  <img src={techimg2} alt="" className="img-fluid" />
+                </div>
+              </div>
+              <span className={`${styles.centeredText}`}>TECH</span>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default DiscoverFeeds
+export default DiscoverFeeds;
